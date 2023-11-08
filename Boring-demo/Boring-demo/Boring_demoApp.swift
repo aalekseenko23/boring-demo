86dccc94bd115144278c52f58dfab7e3c172dcac
aalekseenko23 <148762775+aalekseenko23@users.noreply.github.com>
2023-11-08 12:23:21 +0100

    Simple Message

---
+++ Boring-demo/Boring-demo/Boring_demoApp.swift
@@ -0,0 +1,17 @@
+//
+//  Boring_demoApp.swift
+//  Boring-demo
+//
+//  Created by Alekseenko Artem on 08/11/23.
+//
+
+import SwiftUI
+
+@main
+struct Boring_demoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
